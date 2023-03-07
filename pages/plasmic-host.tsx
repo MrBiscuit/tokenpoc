f8e4a410d1747d87b31c99d707cb31d9580f1d6d
mrbiscuit <sunshuaiqi@gmail.com>
2023-03-07 12:37:19 +0100

    initial push

---
+++ pages/plasmic-host.tsx
@@ -0,0 +1,10 @@
+
+import * as React from 'react';
+import Script from 'next/script';
+
+import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/host';
+
+export default function PlasmicHost() {
+  return <PlasmicCanvasHost />;
+}
+    
